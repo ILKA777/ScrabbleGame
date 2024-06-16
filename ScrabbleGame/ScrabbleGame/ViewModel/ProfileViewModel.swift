dc1d2e6d9a457ec703766e9bfef2e2b77ec95049
gt005 <karimhamid@yandex.ru>
2024-06-16 15:29:00 +0300

    Реализовано удаление аккаунта

--- ScrabbleGame/ScrabbleGame/ViewModel/ProfileViewModel.swift
+++ ScrabbleGame/ScrabbleGame/ViewModel/ProfileViewModel.swift
@@ -63,8 +63,41 @@ class ProfileViewModel: ObservableObject {
     }
 
     func deleteAccount() {
-        // Handle delete account logic here
-        print("User account deleted")
-        isLoggedIn = false
+        getCurrentUserId { [weak self] userId in
+            guard let self = self, let userId = userId else {
+                print("Failed to get user ID")
+                return
+            }
+            
+            guard let url = URL(string: "\(Constants.serverURL)/user/\(userId.uuidString)") else {
+                print("Invalid URL")
+                return
+            }
+            let currentUser = UserManager.shared.getCurrentUser()
+            var request = URLRequest(url: url)
+            request.httpMethod = "DELETE"
+            request.setValue(Constants.serverApiKey, forHTTPHeaderField: "ApiKey")
+            request.setValue("Bearer \(currentUser.userToken!)", forHTTPHeaderField: "Authorization")
+            
+            URLSession.shared.dataTask(with: request) { data, response, error in
+                DispatchQueue.main.async {
+                    guard let data = data, error == nil else {
+                        print("Error: \(error?.localizedDescription ?? "Unknown error")")
+                        
+                        self.logout()
+                        return
+                    }
+                    
+                    if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 204 {
+                        print("Account deleted successfully")
+                        UserManager.shared.logout()
+                        self.isLoggedIn = false
+                    } else {
+                        print("HTTP Error: \(response.debugDescription)")
+                        return
+                    }
+                }
+            }.resume()
+        }
     }
 }
